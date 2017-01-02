f7f266ffe8fa65596d992692578e4966419724b6
chen86860@126.com <chen86860@126.com>
2017-01-02 11:51:22 +0800

    fix some

--- 【1】修复邑大形式与政策网站/index.js
+++ 【1】修复邑大形式与政策网站/index.js
@@ -1,19 +1,67 @@
+/*
+ 五邑大学形式与政策登陆页(http://jpkc.wyu.edu.cn/xsyzc/stlogon/index.html)修复
+
+1.重构代码，修复浏览器兼容性问题，能在Chrome,Firefox,Safari测试通过
+2.解决复制限制
+
+*/
+
 var myID = "6221";
 var lPath = "http://jwc.wyu.cn/logonuser/";
 var bPage = "st_logon.asp";
 var iCode = "code.asp";
 var pPage = "st_login.asp";
 
+loadlogon();
 
-loadlogon()
-
-function loadlogon() {
-    var lgUrl = lPath + bPage,
-        rsp = rspHttp(lgUrl);
-    if (rsp.slice(1, 6) == "table") {
+function chkLogonForm() {
+    var login_form = {};
+    login_form.u = document.getElementsByName("u")[0];
+    login_form.p = document.getElementsByName("p")[0];
+    login_form.v = document.getElementsByName("v")[0];
+    login_form.elements = document.getElementsByTagName("input");
+    if (login_form.u.value == "") {
+        alert("请输入学号！");
+        login_form.u.focus();
+        return false;
+    }
+    if (login_form.u.value.length > 10 || login_form.u.value.length < 8) {
+        alert("请输入正确的学号！");
+        login_form.u.focus();
+        return false;
+    }
+    if (login_form.p.value == "") {
+        alert("请输入密码！");
+        login_form.p.focus();
+        return false;
+    }
+    if (login_form.v.value.length != 4) {
+        alert("请输入验证码！");
+        login_form.v.focus();
+        return false;
+    }
+    var pStr = "";
+    var nm = "";
+    var vl = "";
+    for (var i = 0; i < login_form.elements.length; i++) {
+        nm = login_form.elements[i].name;
+        vl = login_form.elements[i].value;
+        if (i > 0) pStr = pStr + "@@";
+        pStr = pStr + nm + "~~" + escape(vl);
+    }
+    var rsp = postHttp(lPath + pPage, pStr); //lPath+pPage="http://jwc.wyu.cn/logonuser/st_login.asp";
+    if (rsp != "Error") {
         var obj = document.getElementById("stlogon");
-        obj.innerHTML = rsp;
+        obj.innerHTML = '<form  name="myloginform" action="mylogon.asp" method="post"><input type="hidden" name="vd" value=""></form>';
+        var my_form = document.getElementsByName("myloginform")[0];
+        document.getElementsByName("vd")[0].value = rsp;
+        my_form.submit();
+        return true;
+    } else {
+        alert("登录错误！");
+        login_form.v.value = "";
         loadImg();
+        return false;
     }
 }
 
@@ -25,104 +73,50 @@ function loadImg() {
     idd.value = myID;
 }
 
-
-function chkLogonForm() {
-    var stlogon = document.getElementById("stlogon"),
-        login_form = stlogon.getElementsByTagName("input"),
-        login_u = document.getElementById("u"),
-        login_p = document.getElementById("p"),
-        login_v = document.getElementById("v"),
-        pStr = "",
-        nm = "",
-        vl = "",
-        rsp;
-
-    if (!login_u.value) {
-        alert("请输入学号！");
-        login_u.focus();
-        return false;
-    }
-    if (login_u.value.length > 10 || login_u.value.length < 8) {
-        alert("请输入正确的学号！");
-        login_u.focus();
-        return false;
-    }
-    if (!login_p.value) {
-        alert("请输入密码！");
-        login_p.focus();
-        return false;
-    }
-    if (login_v.value.length != 4) {
-        alert("请输入验证码！");
-        login_v.focus();
-        return false;
-    }
-
-    for (var i = 0; i < login_form.length; i++) {
-        nm = login_form[i].name;
-        vl = login_form[i].value;
-        if (i > 0)
-            pStr = pStr + "@@";
-        pStr = pStr + nm + "~~" + escape(vl);
-    }
-
-    rsp = postHttp(lPath + pPage, pStr);
-
-    if (rsp != "Error") {
+function loadlogon() {
+    var lgUrl = lPath + bPage;
+    var rsp = rspHttp(lgUrl);
+    if (rsp.slice(1, 6) == "table") {
         var obj = document.getElementById("stlogon");
-        obj.innerHTML = '<form  name="myloginform" action="mylogon.asp" method="post"><input type="hidden" name="vd" value=""></form>';
-        var my_form = document.getElementsByName("myloginform")[0],
-            vd = document.getElementsByName("vd")[0];
-        vd.value = rsp;
-        my_form.submit();
-        return true;
-    } else {
-        alert("登录错误！");
-        login_v.value = "";
+        obj.innerHTML = rsp;
         loadImg();
-        return false;
     }
 }
 
 function rspHttp(nUrl) {
-    var my_url = "getpage.asp?url=" + nUrl,
-        xmldoc = createXMLHttp();
+    var my_url = "getpage.asp?url=" + escape(nUrl);
+    var xmldoc = (window.XMLHttpRequest ?
+        new XMLHttpRequest() :
+        new ActiveXObject("Microsoft.XMLHTTP"));
     xmldoc.open("POST", my_url, false);
     xmldoc.send();
     return unescape(xmldoc.responseText);
 }
 
 function postHttp(nUrl, postData) {
-    var my_url = "postpage.asp?url=" + escape(nUrl) + "::::" + postData,
-        xmldoc = createXMLHttp();
+    var my_url = "postpage.asp?url=" + escape(nUrl) + "::::" + postData;
+    var xmldoc = (window.XMLHttpRequest ?
+        new XMLHttpRequest() :
+        new ActiveXObject("Microsoft.XMLHTTP"));
     xmldoc.open("POST", my_url, false);
     xmldoc.send();
     return unescape(xmldoc.responseText);
 }
 
-//兼容多种浏览器的XMLHTTP
-function createXMLHttp() {
-    var XmlHttp;
-    if (window.ActiveXObject) {
-        var arr = ["MSXML2.XMLHttp.6.0", "MSXML2.XMLHttp.5.0", "MSXML2.XMLHttp.4.0", "ML2.XMLHttp .3 .0 ", "MSXML2.XMLHttp ", "Microsoft.XMLHttp "];
-        for (var i = 0; i < arr.length; i++) {
-            try {
-                XmlHttp = new ActiveXObject(arr[i]);
-                return XmlHttp;
-            } catch (error) {}
-        }
-    } else {
-        try {
-            XmlHttp = new XMLHttpRequest();
-            return XmlHttp;
-        } catch (otherError) {
 
-        }
-    }
-}
+/*
+	解决右键复制限制
+*/
 
-window.addEventListener("keyup", function(e) {
-    if (e.keyCode == 13) {
-        chkLogonForm()
-    }
-}, false);
+// var frame
+// try {
+//     if (!document.getElementById("main")) {
+//         frame = document.getElementById("main")
+//     } else {
+//         frame = window.frames["main"]
+//     }
+//     frame.contentWindow.document.body.onselectstart = ""
+//     frame.contentWindow.document.body.oncontextmenu = ""
+// } catch (e) {
+//     console.log("Something error happened...")
+// }
